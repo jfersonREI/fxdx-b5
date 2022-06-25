9153081d6304a50e4a3c8d69f999814969d464c4
jfersonREI <jeff.ferson@reisystems.com>
2022-06-25 05:22:11 -0400

    btn

--- assets/js/app-favorite-btn-toggle.js
+++ assets/js/app-favorite-btn-toggle.js
@@ -1,7 +1,18 @@
 const favBtn = document.querySelector(".data-card-fav-btn");
+let is_favorite = false;
 
 let favToggle = () => {
-  favBtn.classList.toggle("data-card-fav-btn--favorited");
+  if (!is_favorite) {
+    favBtn.classList.add("data-card-fav-btn--favorited");
+    favBtn.setAttribute("aria-label", "Unfavorite");
+    is_favorite = true;
+    console.log(is_favorite);
+  } else {
+    favBtn.classList.remove("data-card-fav-btn--favorited");
+    favBtn.setAttribute("aria-label", "Favorite");
+    is_favorite = false;
+    console.log(is_favorite);
+  }
 };
 
 favBtn.addEventListener("click", favToggle);
